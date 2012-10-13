fbc1fe136f9d72645aef3fb5170fa9b5e2184709
Alex Norman <x37v.alex@gmail.com>
2012-10-13 16:42:59 -0700

    fixed up tabbing

--- main.c
+++ main.c
@@ -43,60 +43,58 @@ ISR(TIM0_COMPA_vect) {
 }
 
 int main(void) {
-   wdt_disable();
+  wdt_disable();
 
-   //LED OUT
-   DDRA |= _BV(LED_R_PIN) | _BV(LED_G_PIN) | _BV(LED_B_PIN);
+  //LED OUT
+  DDRA |= _BV(LED_R_PIN) | _BV(LED_G_PIN) | _BV(LED_B_PIN);
 
-   //igniter
-   DDRA |= _BV(PINA0);
+  //igniter
+  DDRA |= _BV(PINA0);
 
-   //set up timer 0
-   TCCR0A = 0; //normal mode
-   TCCR0B = _BV(CS00); //no prescale
-   OCR0A = 16;
+  //set up timer 0
+  TCCR0A = 0; //normal mode
+  TCCR0B = _BV(CS00); //no prescale
+  OCR0A = 16;
 
-   //TIMSK0 = _BV(TOIE0); //enable interrupts
-   TIMSK0 = _BV(OCIE0A); //enable interrupts for output compare match a
-   TIFR0 = _BV(OCF0A);
+  //TIMSK0 = _BV(TOIE0); //enable interrupts
+  TIMSK0 = _BV(OCIE0A); //enable interrupts for output compare match a
+  TIFR0 = _BV(OCF0A);
 
-   //set up timer 1
-   TCCR1A = 0;
-   TCCR1B = 0; 
-   TCCR1C = 0; 
-   TCNT1L = 0;
-   OCR1AL = 0xF;
-   
-   //toggles OC1[A,B] when the timer reaches the top
-   //TCCR1A = _BV(COM1A0) |  _BV(COM1B0);
-   TCCR1A = _BV(COM1A1) | _BV(COM1A0) |  _BV(COM1B0);
+  //set up timer 1
+  TCCR1A = 0;
+  TCCR1B = 0; 
+  TCCR1C = 0; 
+  TCNT1L = 0;
+  OCR1AL = 0xF;
 
-   //CTC mode, resets clock when it meets OCR1A
-   TCCR1B = _BV(WGM12) | _BV(CS10) | _BV(CS11);
+  //toggles OC1[A,B] when the timer reaches the top
+  //TCCR1A = _BV(COM1A0) |  _BV(COM1B0);
+  TCCR1A = _BV(COM1A1) | _BV(COM1A0) |  _BV(COM1B0);
 
-   TCCR1C = _BV(FOC1A); //force one of the outputs to high
+  //CTC mode, resets clock when it meets OCR1A
+  TCCR1B = _BV(WGM12) | _BV(CS10) | _BV(CS11);
 
-   //set up output PWM
-   DDRA |= _BV(PINA6) | _BV(PINA5);
+  TCCR1C = _BV(FOC1A); //force one of the outputs to high
 
-   TIMSK1 = 0;
+  //set up output PWM
+  DDRA |= _BV(PINA6) | _BV(PINA5);
 
-   //PORTA |= _BV(LED_B_PIN);
+  TIMSK1 = 0;
 
-   sei();
+  sei();
 
-   uint8_t r = 0;
-   uint8_t g = 23;
-   uint8_t b = 64;
-   while(1) {
-     //_delay_ms(1);
-     r++;
-     g++;
-     b++;
+  uint8_t r = 0;
+  uint8_t g = 23;
+  uint8_t b = 64;
+  while(1) {
+    //_delay_ms(1);
+    r++;
+    g++;
+    b++;
 
-     led_r_off = gamma_correct(r);
-     led_g_off = gamma_correct(g);
-     led_b_off = gamma_correct(b);
-   }
+    led_r_off = gamma_correct(r);
+    led_g_off = gamma_correct(g);
+    led_b_off = gamma_correct(b);
+  }
 }
 
