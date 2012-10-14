d206109cbdfeaab428c8dd0a20a23e02a79fd6f5
Alex Norman <x37v.alex@gmail.com>
2012-10-13 17:45:11 -0700

    button works now

--- main.c
+++ main.c
@@ -11,10 +11,12 @@
 #include <avr/wdt.h>
 #include "gamma.h"
 
-//verify
 #define LED_R_PIN PINA4
 #define LED_G_PIN PINA3
 #define LED_B_PIN PINA2
+
+#define BUTTON_PIN PINA7
+#define IGNITE_PIN PINA0
 
 volatile uint8_t led_count = 0;
 
@@ -42,24 +44,7 @@ ISR(TIM0_COMPA_vect) {
     PORTA |= _BV(LED_B_PIN);
 }
 
-int main(void) {
-  wdt_disable();
-
-  //LED OUT
-  DDRA |= _BV(LED_R_PIN) | _BV(LED_G_PIN) | _BV(LED_B_PIN);
-
-  //igniter
-  DDRA |= _BV(PINA0);
-
-  //set up timer 0
-  TCCR0A = 0; //normal mode
-  TCCR0B = _BV(CS00); //no prescale
-  OCR0A = 16;
-
-  //TIMSK0 = _BV(TOIE0); //enable interrupts
-  TIMSK0 = _BV(OCIE0A); //enable interrupts for output compare match a
-  TIFR0 = _BV(OCF0A);
-
+void enable_buzzer(void) {
   //set up timer 1
   TCCR1A = 0;
   TCCR1B = 0; 
@@ -76,25 +61,67 @@ int main(void) {
 
   TCCR1C = _BV(FOC1A); //force one of the outputs to high
 
+  TIMSK1 = 0;
+}
+
+void init_timer0(void) {
+  //set up timer 0
+  TCCR0A = 0; //normal mode
+  TCCR0B = _BV(CS00); //no prescale
+  OCR0A = 16;
+
+  //TIMSK0 = _BV(TOIE0); //enable interrupts
+  TIMSK0 = _BV(OCIE0A); //enable interrupts for output compare match a
+  TIFR0 = _BV(OCF0A);
+}
+
+void init_io(void) {
+  //enable pullups
+  MCUCR &= ~_BV(PUD);
+
+  DDRA = 0;
+
+  //LED OUT
+  DDRA |= _BV(LED_R_PIN) | _BV(LED_G_PIN) | _BV(LED_B_PIN);
+
+  //igniter
+  DDRA |= _BV(IGNITE_PIN);
+
   //set up output PWM
   DDRA |= _BV(PINA6) | _BV(PINA5);
 
-  TIMSK1 = 0;
+  //button is input [0], set pullup
+  PORTA |= _BV(BUTTON_PIN);
+}
+
+int main(void) {
+  wdt_disable();
+
+  init_io();
+  init_timer0();
+  enable_buzzer();
 
   sei();
 
   uint8_t r = 0;
   uint8_t g = 23;
   uint8_t b = 64;
+  //PORTA |= _BV(LED_R_PIN);
   while(1) {
-    //_delay_ms(1);
-    r++;
-    g++;
-    b++;
-
-    led_r_off = gamma_correct(r);
-    led_g_off = gamma_correct(g);
-    led_b_off = gamma_correct(b);
+    if (!(PINA & _BV(BUTTON_PIN))) {
+      led_r_off = 255;
+      ////_delay_ms(1);
+      //r++;
+      //g++;
+      //b++;
+      //led_r_off = gamma_correct(r);
+      //led_g_off = gamma_correct(g);
+      //led_b_off = gamma_correct(b);
+      //PORTA |= _BV(LED_B_PIN);
+    } else {
+      led_r_off = 0;
+      //PORTA &= ~(_BV(LED_B_PIN));
+    }
   }
 }
 
