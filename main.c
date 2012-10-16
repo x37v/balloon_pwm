9bb607adda322f1f060f902297c690a7c1c723da
Alex Norman <x37v.alex@gmail.com>
2012-10-15 21:18:14 -0700

    have a program time concept working..

--- main.c
+++ main.c
@@ -34,42 +34,55 @@ volatile uint8_t led_b_off = 0;
 
 volatile uint8_t led_timeout = 0;
 
-#define NUM_COLORSETS 8
-uint8_t color_sets[NUM_COLORSETS * 3] = {
-  0, 255, 0,
+volatile uint16_t rollover_count = 0;
+volatile uint8_t rollover = FALSE;
+
+
+#define FOUR_BIT_LED
+
+#define ROLLOVER_INC 64
+
+#define NUM_COLORSETS 5
+static uint8_t color_sets[NUM_COLORSETS * 3] = {
   255, 255, 0,
-  0, 255, 255,
+  255, 0, 0,
+  255, 0, 170,
   0, 255, 0,
-  0, 1, 65,
-  65, 65, 2,
-  23, 0, 59,
-  255, 0, 0
+  0, 1, 255
 };
+
+static uint8_t colors_off[3] = {0, 0, 0};
 
 void enable_buzzer(uint8_t v);
 void disable_buzzer(void);
 
-//ISR(TIM0_OVF_vect) {
+#ifdef FOUR_BIT_LED
+ISR(TIM0_OVF_vect) {
+#else
 ISR(TIM0_COMPA_vect) {
+#endif
   TCNT0 = 0;
+
+#ifdef FOUR_BIT_LED
+  led_count += 16;
+  rollover_count += ROLLOVER_INC;
+#else
   led_count++;
+  rollover_count += ROLLOVER_INC >> 4;
+#endif
 
-  if (led_count == 0) {
-    if (led_timeout != 0 && --led_timeout == 0) {
-      led_r_off = led_g_off = led_b_off = 0;
-      //disable_buzzer();
-    }
-  }
+  if (rollover_count == 0)
+    rollover = TRUE;
 
-  if (led_count == led_r_off)
+  if (led_count >= led_r_off)
     PORTA &= ~(_BV(LED_R_PIN));
   else if (led_count == 0)
     PORTA |= _BV(LED_R_PIN);
-  if (led_count == led_g_off)
+  if (led_count >= led_g_off)
     PORTA &= ~(_BV(LED_G_PIN));
   else if (led_count == 0)
     PORTA |= _BV(LED_G_PIN);
-  if (led_count == led_b_off)
+  if (led_count >= led_b_off)
     PORTA &= ~(_BV(LED_B_PIN));
   else if (led_count == 0)
     PORTA |= _BV(LED_B_PIN);
@@ -104,10 +117,14 @@ void disable_buzzer(void) {
 void init_timer0(void) {
   TCCR0A = 0; //normal mode
   TCCR0B = _BV(CS00); //no prescale
+#ifdef FOUR_BIT_LED
+  //OCR0A = 16 << 4;
+  TIMSK0 = _BV(TOIE0); //enable interrupts
+#else
   OCR0A = 16;
-
-  //TIMSK0 = _BV(TOIE0); //enable interrupts
   TIMSK0 = _BV(OCIE0A); //enable interrupts for output compare match a
+#endif
+
   TIFR0 = _BV(OCF0A);
 }
 
@@ -138,13 +155,22 @@ inline void set_led(uint8_t* rgb, uint8_t timeout) {
   led_g_off = *(rgb + 1);
   led_b_off = *(rgb + 2);
   led_count = 0;
+#ifdef FOUR_BIT_LED
+  led_timeout = timeout >> 4;
+  if (!led_timeout && timeout)
+    led_timeout = 1;
+#else
   led_timeout = timeout;
+#endif
 }
 
 int main(void) {
   static uint8_t button_down_index = 0;
   static uint8_t button_down_history = 0;
   static uint8_t button_down_last = FALSE;
+  static uint8_t color_idx = 0;
+
+  static uint32_t program_time = 0;
 
   wdt_disable();
 
@@ -173,6 +199,23 @@ int main(void) {
 
       set_led(color_sets + 3 * (rand() % NUM_COLORSETS), 16);
     }
+
+    if (rollover) {
+      program_time++;
+
+      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        rollover = FALSE;
+        rollover_count = 0;
+      }
+      
+      if (program_time % 32 == 0) {
+        color_idx = (color_idx + 1) % NUM_COLORSETS;
+        set_led(color_sets + 3 * color_idx, 16);
+      } else {
+        set_led(colors_off, 16);
+      }
+    }
+
     //led_r_off = gamma_correct(r);
     //led_g_off = gamma_correct(g);
     //led_b_off = gamma_correct(b);
