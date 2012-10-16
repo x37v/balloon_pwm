f787aa5be0db53296665a5a6422d98dbc24e647a
Alex Norman <x37v.alex@gmail.com>
2012-10-15 23:21:01 -0700

    working on stages.. more flexible buzzer setup too

--- main.c
+++ main.c
@@ -12,6 +12,10 @@
 #include <util/atomic.h>
 #include "gamma.h"
 
+#define STAGE_COUNTS 16
+#define PROGRAM_SUBDIV 32 
+#define STAGE_BPRESS_THRESH 16
+
 #define LED_R_PIN PINA4
 #define LED_G_PIN PINA3
 #define LED_B_PIN PINA2
@@ -32,8 +36,6 @@ volatile uint8_t led_r_off = 0;
 volatile uint8_t led_g_off = 0;
 volatile uint8_t led_b_off = 0;
 
-volatile uint8_t led_timeout = 0;
-
 volatile uint16_t rollover_count = 0;
 volatile uint8_t rollover = FALSE;
 
@@ -53,7 +55,7 @@ static uint8_t color_sets[NUM_COLORSETS * 3] = {
 
 static uint8_t colors_off[3] = {0, 0, 0};
 
-void enable_buzzer(uint8_t v);
+void enable_buzzer(uint16_t v);
 void disable_buzzer(void);
 
 #ifdef FOUR_BIT_LED
@@ -102,11 +104,12 @@ void init_buzzer() {
   TIMSK1 = 0;
 }
 
-void enable_buzzer(uint8_t v) {
+void enable_buzzer(uint16_t v) {
   //toggles OC1[A,B] when the timer reaches the top
   TCCR1A = _BV(COM1A1) | _BV(COM1A0) |  _BV(COM1B0);
   TCCR1C = _BV(FOC1A); //force one of the outputs to high
-  OCR1AL = v;
+  OCR1AH = v >> 8;
+  OCR1AL = v & 0xFF;
   TCNT1 = 0;
 }
 
@@ -150,25 +153,65 @@ void init_io(void) {
   PORTA |= _BV(BUTTON_PIN);
 }
 
-inline void set_led(uint8_t* rgb, uint8_t timeout) {
+inline void set_led(uint8_t* rgb) {
   led_r_off = *rgb;
   led_g_off = *(rgb + 1);
   led_b_off = *(rgb + 2);
-  led_count = 0;
-#ifdef FOUR_BIT_LED
-  led_timeout = timeout >> 4;
-  if (!led_timeout && timeout)
-    led_timeout = 1;
-#else
-  led_timeout = timeout;
-#endif
+}
+
+static uint8_t stage = 0;
+static uint8_t stage_subdiv = 0;
+static uint8_t stage_bpress = 0;
+static uint8_t stage_new = TRUE;
+static uint8_t stage_state = 0;
+static uint32_t program_timeout = 0;
+static uint32_t led_timeout = 0;
+static uint8_t color_idx = 0;
+
+void update_stage() {
+  stage++;
+  stage_subdiv = 0;
+  stage_bpress = 0;
+  stage_new = TRUE;
+}
+
+void exec_stage(uint32_t program_time) {
+  switch(stage) {
+    case 0:
+      if (stage_new)
+        stage_state = 0;
+
+      if (stage_new || program_time == program_timeout) {
+        if (stage_state == 0) {
+          color_idx = (color_idx + 1) % NUM_COLORSETS;
+          set_led(color_sets + 3 * color_idx);
+          enable_buzzer(((rand() & 0xF) + 1) << 10);
+          program_timeout = program_time + 2 + (rand() & 0x3F);
+          led_timeout = program_time + 1;
+          if (rand() & 0xFF >= 75) //66% of the time turn off
+            stage_state = 1;
+        } else {
+          disable_buzzer();
+          program_timeout = program_time + 8 + (rand() & 0xF);
+          stage_state = 0;
+        }
+      }
+
+      if (program_time == led_timeout)
+        set_led(colors_off);
+      break;
+    case 4:
+    default:
+      break;
+  }
+
+  stage_new = FALSE;
 }
 
 int main(void) {
   static uint8_t button_down_index = 0;
   static uint8_t button_down_history = 0;
   static uint8_t button_down_last = FALSE;
-  static uint8_t color_idx = 0;
 
   static uint32_t program_time = 0;
 
@@ -195,30 +238,29 @@ int main(void) {
       //button is now down
       button_down_last = TRUE;
 
-      enable_buzzer(rand());
-
-      set_led(color_sets + 3 * (rand() % NUM_COLORSETS), 16);
+      /*
+      //update 
+      stage_bpress++;
+      if (stage_bpress >= STAGE_BPRESS_THRESH)
+        update_stage();
+        */
+      stage_new = TRUE;
     }
 
+    //deal with timed events
     if (rollover) {
       program_time++;
-
       ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         rollover = FALSE;
         rollover_count = 0;
       }
-      
-      if (program_time % 32 == 0) {
-        color_idx = (color_idx + 1) % NUM_COLORSETS;
-        set_led(color_sets + 3 * color_idx, 16);
-      } else {
-        set_led(colors_off, 16);
+      if (program_time % PROGRAM_SUBDIV == 0) {
+        stage_subdiv++;
+        if (stage_subdiv >= STAGE_COUNTS)
+          update_stage();
       }
+      exec_stage(program_time);
     }
-
-    //led_r_off = gamma_correct(r);
-    //led_g_off = gamma_correct(g);
-    //led_b_off = gamma_correct(b);
   }
 }
 
